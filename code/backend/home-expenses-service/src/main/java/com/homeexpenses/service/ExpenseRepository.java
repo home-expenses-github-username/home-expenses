4c49986111d0a0fafaaa0a1cdf28905e196f5b60
Ilnur Nigmatzyanov <ilnur.nigma@gmail.com>
2023-02-05 22:16:49 +0100

    Add allow cross origin

--- code/backend/home-expenses-service/src/main/java/com/homeexpenses/service/ExpenseRepository.java
+++ code/backend/home-expenses-service/src/main/java/com/homeexpenses/service/ExpenseRepository.java
@@ -3,6 +3,8 @@ package com.homeexpenses.service;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ExpenseRepository extends PagingAndSortingRepository<Expense, Long>, CrudRepository<Expense, Long> {
 }
