5301026eab62ece96bd3ff4151f6646a6a3ddd23
unlimit1984 <unlimit_rmc@mail.ru>
2023-02-11 21:25:48 +0100

    Add `angular-cli-ghpages` for CI/CD

--- code/frontend/home-expenses-ui/src/app/services/http/expenses.service.ts
+++ code/frontend/home-expenses-ui/src/app/services/http/expenses.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { interval, map, Observable} from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 import { Expense } from '../../interfaces/Expense';
 
 const expensesMock: Expense[] = require('../../mock/expenses-mock.json');
@@ -12,16 +12,16 @@ export class ExpensesService {
   constructor(private httpClient: HttpClient) {}
 
   public getExpenses(): Observable<Expense[]> {
-    return this.httpClient
-      // .get<{ _embedded: {expenses: Expense[]} }>('http://localhost:8080/api/expenses')
-      .get<{ _embedded: {expenses: Expense[]} }>('/api/expenses')
-      .pipe(map((response) => response._embedded.expenses));
+    // return this.httpClient
+    // .get<{ _embedded: {expenses: Expense[]} }>('http://localhost:8080/api/expenses')
+    // .get<{ _embedded: {expenses: Expense[]} }>('/api/expenses')
+    // .pipe(map((response) => response._embedded.expenses));
 
     // return of(expensesMock);
-    // return interval(2000).pipe(map(() => expensesMock));
+    return interval(2000).pipe(map(() => expensesMock));
   }
 
-  public createExpense(expenseBody: Expense): Observable<any> {
-    return this.httpClient.post<any>('http://localhost:8080/api/expenses', expenseBody);
+  public createExpense(expenseBody: Expense): Observable<Expense> {
+    return this.httpClient.post<Expense>('http://localhost:8080/api/expenses', expenseBody);
   }
 }
