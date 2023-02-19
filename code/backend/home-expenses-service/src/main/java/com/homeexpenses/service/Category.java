17e4aea429572e5e1867b728b63d525966cc53b7
Ilnur Nigmatzyanov <ilnur.nigma@gmail.com>
2023-02-19 16:53:46 +0100

    Decrease spring boot and java versions

--- code/backend/home-expenses-service/src/main/java/com/homeexpenses/service/Category.java
+++ code/backend/home-expenses-service/src/main/java/com/homeexpenses/service/Category.java
@@ -1,10 +1,12 @@
 package com.homeexpenses.service;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Entity
 @Getter
--- code/backend/home-expenses-service/src/main/java/com/homeexpenses/service/Expense.java
+++ code/backend/home-expenses-service/src/main/java/com/homeexpenses/service/Expense.java
@@ -1,12 +1,12 @@
 package com.homeexpenses.service;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.ZonedDateTime;
 
 @Entity
