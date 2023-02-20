a1622de6bf09d324703b3359e8d0d7c5128752a4
unlimit1984 <unlimit_rmc@mail.ru>
2023-02-21 00:02:07 +0100

    Add frontend url to cors

--- code/backend/home-expenses-service/src/main/java/com/homeexpenses/service/ExpenseRepository.java
+++ code/backend/home-expenses-service/src/main/java/com/homeexpenses/service/ExpenseRepository.java
@@ -2,9 +2,9 @@ package com.homeexpenses.service;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200")
+//@CrossOrigin("http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "https://yellow-bush-03266d003.2.azurestaticapps.net"})
 public interface ExpenseRepository extends PagingAndSortingRepository<Expense, Long>, CrudRepository<Expense, Long> {
 }
