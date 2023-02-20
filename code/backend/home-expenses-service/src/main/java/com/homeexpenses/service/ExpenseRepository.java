e263ef5ba3387f4a2bf1fcca7a6f18089df2a222
unlimit1984 <unlimit_rmc@mail.ru>
2023-02-21 00:08:50 +0100

    Test CORS - 1

--- code/backend/home-expenses-service/src/main/java/com/homeexpenses/service/ExpenseRepository.java
+++ code/backend/home-expenses-service/src/main/java/com/homeexpenses/service/ExpenseRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 //@CrossOrigin("http://localhost:4200")
-@CrossOrigin(origins = {"http://localhost:4200", "https://yellow-bush-03266d003.2.azurestaticapps.net"})
+//@CrossOrigin(origins = {"http://localhost:4200", "https://yellow-bush-03266d003.2.azurestaticapps.net"})
 public interface ExpenseRepository extends PagingAndSortingRepository<Expense, Long>, CrudRepository<Expense, Long> {
 }
