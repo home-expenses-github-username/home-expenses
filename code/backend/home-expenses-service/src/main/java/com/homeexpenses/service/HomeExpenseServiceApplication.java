e948d70485d7f03914dc4354581abf69a2b22792
unlimit1984 <unlimit_rmc@mail.ru>
2023-03-12 00:41:37 +0100

    Use secrets as database credentials

--- code/backend/home-expenses-service/src/main/java/com/homeexpenses/service/HomeExpenseServiceApplication.java
+++ code/backend/home-expenses-service/src/main/java/com/homeexpenses/service/HomeExpenseServiceApplication.java
@@ -2,6 +2,11 @@ package com.homeexpenses.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class HomeExpenseServiceApplication {
