e1f849a0225f5607db58bb9a5f72c79abf16cc5d
unlimit1984 <unlimit_rmc@mail.ru>
2023-05-03 22:06:21 +0200

    Try Error #22

--- code/backend/home-expenses-auth/src/app.service.ts
+++ code/backend/home-expenses-auth/src/app.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('hello service');;
+    console.log('hello service');;;
 
-    return 'Hello World!';;
+    return 'Hello World!';;;
   }
 }
