486b260e5a49e1c77df6cd341867e4b40aeee960
unlimit1984 <unlimit_rmc@mail.ru>
2023-05-19 22:29:23 +0200

    [auth] Add mailer options

--- code/backend/home-expenses-auth/src/app.module.ts
+++ code/backend/home-expenses-auth/src/app.module.ts
@@ -13,49 +13,10 @@ import { MailService } from './services/mailer/mail.service';
 import { MailController } from './controllers/mail/mail.controller';
 import { TestService } from './services/test/test.service';
 import { TestController } from './controllers/test/test.controller';
+import { mailerOptions } from './services/mailer/mail.google.settings';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    MailerModule.forRoot({
-      // transport: 'smtp://user@example.com:topsecret@smtp.example.com:1025',
-      // transport: {
-      //   host: 'localhost',
-      //   port: 1025,
-      //   ignoreTLS: true,
-      //   secure: false
-      // },
-      // transport: {
-      //   host: 'smtp.example.com',
-      //   ignoreTLS: true,
-      //   // host: 'localhost',
-      //   // port: 1025,
-      //   secure: false, // true for 465, false for other ports
-      //   auth: {
-      //     user: 'user@example.com', // generated ethereal user
-      //     pass: 'topsecret' // generated ethereal password
-      //   }
-      // },
-      transport: {
-        host: 'smtp.gmail.com',
-        auth: {
-          user: 'home.expenses.user@gmail.com',
-          pass: ''
-        }
-      },
-      defaults: {
-        from: '"nest-modules" <noreply@example.com>'
-      }
-      // template: {
-      //   // dir: __dirname + '/templates',
-      //   dir: __dirname + '/templates',
-      //   adapter: new PugAdapter(),
-      //   options: {
-      //     strict: true
-      //   }
-      // }
-    })
-  ],
+  imports: [DatabaseModule, MailerModule.forRoot(mailerOptions)],
   controllers: [UserController, MailController, TestController],
   providers: [UserDbService, ...userProviders, MailService, TestService]
 })
